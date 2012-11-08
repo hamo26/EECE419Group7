e3864d4b36d840e243fd6e2a874db1062d602ed0
Vinny <ubcvincenttsui@gmail.com>
2012-11-07 21:57:57 -0800

    added FB web framework

---
+++ schedushare-web/application/controllers/main.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of main
+ *
+ * @author Cat
+ */
+class Main extends CI_Controller {
+    //put your code here
+    function index(){
+        $this->load->view("main_page");
+    }
+}
+
+?>
---
+++ schedushare-web/application/views/main_page.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "this is the main page";
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
