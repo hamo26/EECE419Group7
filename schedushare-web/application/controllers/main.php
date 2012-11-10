7f4f5cf6607df3de7b421fb4502d29f9cb237aac
Vinny <ubcvincenttsui@gmail.com>
2012-11-10 15:07:12 -0800

    testing fb token

--- schedushare-web/application/controllers/main.php
+++ schedushare-web/application/controllers/main.php
@@ -18,6 +18,20 @@ class Main extends CI_Controller {
         //load classes
         $this->load->helper('url');
         
+        $app_id = "244995238959505";
+        $app_secret = "d47346c1d60a1b60df939ef5464b28a2";
+        $app_token_url = "https://graph.facebook.com/oauth/access_token?"
+            . "client_id=" . $app_id
+            . "&client_secret=" . $app_secret 
+            . "&grant_type=client_credentials";
+
+            $response = file_get_contents($app_token_url);
+            $params = null;
+        parse_str($response, $params);
+
+        echo("This app's access token is: " . $params['access_token']);
+        
+        
         $data['title'] = "Sched-u-Share";
         
         //load view
